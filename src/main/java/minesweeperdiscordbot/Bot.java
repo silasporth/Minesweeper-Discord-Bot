6d129921d9253b5836ab15fa3d2587958c96cdc8
Lavariator <lavariator74@gmail.com>
2021-02-16 13:11:14 +0100

    Changed Bot Creation

--- src/main/java/minesweeperdiscordbot/Bot.java
+++ src/main/java/minesweeperdiscordbot/Bot.java
@@ -1,6 +1,5 @@
 package minesweeperdiscordbot;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -13,8 +12,7 @@ import java.nio.file.Paths;
 
 public class Bot {
     public static void main(String[] args) throws LoginException, IOException {
-        JDABuilder builder = new JDABuilder(AccountType.BOT);
-        builder.setToken(new String(Files.readAllBytes(Paths.get("token.txt"))));
+        JDABuilder builder = JDABuilder.createDefault(new String(Files.readAllBytes(Paths.get("token.txt"))));
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.playing("!minesweeper info"));
         builder.addEventListeners(new CommandListener());
